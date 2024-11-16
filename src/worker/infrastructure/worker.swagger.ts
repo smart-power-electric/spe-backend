06404440d19fff025b8ee85e9a1d71f32590b0c6
raymelguerra <raymelguerra@gmail.com>
2024-11-16 02:02:11 -0600

    rollback

--- src/worker/infrastructure/worker.swagger.ts
+++ src/worker/infrastructure/worker.swagger.ts
@@ -41,13 +41,13 @@ export class CreateWorkerRequest implements CreateWorkerDto {
   })
   socialSecurity: string | null;
   @ApiProperty({
-    type: 'date-time',
+    type: Date,
     description: 'Start date contract of the worker',
     nullable: true,
   })
   startDate: Date | null;
   @ApiProperty({
-    type: 'date-time',
+    type: Date,
     description: 'End date contract of the worker',
     nullable: true,
   })
@@ -103,13 +103,13 @@ export class UpdateWorkerRequest implements UpdateWorkerDto {
   })
   socialSecurity?: string | null;
   @ApiPropertyOptional({
-    type: 'date-time',
+    type: Date,
     description: 'State of the worker',
     nullable: true,
   })
   startDate?: Date | null;
   @ApiPropertyOptional({
-    type: 'date-time',
+    type: Date,
     description: 'Zip of the worker',
     nullable: true,
   })
@@ -171,13 +171,13 @@ export class WorkerResponse implements WorkerRow {
   })
   socialSecurity: string | null;
   @ApiProperty({
-    type: 'date-time',
+    type: Date,
     description: 'State of the worker',
     nullable: true,
   })
   startDate: Date | null;
   @ApiProperty({
-    type: 'date-time',
+    type: Date,
     description: 'Zip of the worker',
     nullable: true,
   })
